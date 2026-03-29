c1ee4d2b87b555053af4ef87b8f7c3b5f58c1175
calluna-lt3 <106458302+calluna-lt3@users.noreply.github.com>
2026-03-29 02:53:22 -0700

    fweh

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-idea: pub/priv keys to use service similar to ssh
+file hosting service because every one i ever use dies
+
+for POST reqs:
+* used by friends only
+    * username/password as fields in POST reqs for establishing new
+      connections? then can whitelist the IP afterwards
+* need to block spam
+    * set # of queries before blocked via firewall, idk how to do this
+
+how do we handle page routing?
+* hardcode all knowable paths, e.g. "/", "/styles.css"
+* route ALL NON-DEFINED TRAFFIC to some "/arbitrary/" path so that we can only
+  serve files that we want
+* for now just serve files on the paths that match their names, however in the
+  future we can do a lookup from path -> hashed filename if we want more
+  memorable names
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,16 @@
  *
  */
 
+// NOTE: check check check
+// * https://github.com/theseus-rs/file-type/blob/main/FILETYPES.md
 use std::convert::Infallible;
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::net::SocketAddr;
 
-use futures_util::stream::StreamExt;
 use http_body_util::{BodyExt, Full};
-use hyper::body::Bytes;
+use hyper::body::{Body, Bytes};
+use hyper::header::HeaderValue;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Method, Request, Response};
@@ -21,18 +23,22 @@ use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 use std::net::Ipv4Addr;
 
+const FILESIZE_MAX: u64 = 2048;
+
+// TODO: proper templates
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
 #[macro_export]
 macro_rules! template_content {
     ($x:expr) => {
         match $x {
             404 => { Bytes::from("404") },
-            _ => { panic!("idunno what happened sorry") },
+            _ => { panic!("idunno what happened sorry"); },
         }
     };
 }
 
 
+#[allow(dead_code)]
 fn print_packet(req: &Request<hyper::body::Incoming>) {
     let mut headers = String::new();
     for (k, v) in req.headers() {
@@ -54,8 +60,8 @@ fn print_packet(req: &Request<hyper::body::Incoming>) {
 async fn handle_get(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     let content = match req.uri().path() {
         "/" => {
-            let index = File::open("./index.html");
-            if let Ok(file) = index {
+            let file = File::open("./pages/index.html");
+            if let Ok(file) = file {
                 // TODO: change this to return a 404 if any error exists
                 file.bytes().map(|x| x.unwrap_or(b' ')).collect()
             } else {
@@ -63,36 +69,153 @@ async fn handle_get(req: Request<hyper::body::Incoming>) -> Result<Response<Full
             }
         },
         "/styles.css" => {
-            let styles = File::open("./styles.css");
-            if let Ok(file) = styles {
+            let file = File::open("./pages/styles.css");
+            if let Ok(file) = file {
                 file.bytes().map(|x| x.unwrap_or(b' ')).collect()
             } else {
                 template_content!(404)
             }
         },
-        "/bunny" => {
-            Bytes::from("bunny")
-        },
-        _ => {
-            template_content!(404)
+        other => {
+            let file = File::open(format!("./arbitrary{other}"));
+            if let Ok(file) = file {
+                file.bytes().map(|x| x.unwrap_or(b' ')).collect()
+            } else {
+                template_content!(404)
+            }
         }
     };
 
     Ok(Response::new(Full::new(content)))
 }
 
-// https://www.iana.org/assignments/media-types/media-types.xhtml
+fn boundary_from_content_type(content_type: &HeaderValue) -> Option<String> {
+    let content_type = content_type.to_str().unwrap();
+    let boundary_start = content_type.find("boundary");
+    if let Some(start) = boundary_start {
+        // remove 'boundary=' from the range
+        let start = start + 9;
+        let mut chars = (&content_type[start..]).chars();
+        let mut end = start;
+
+        // find semicolon or EOL
+        loop {
+            let char = chars.next();
+            if let None = char {
+                break;
+            }
+
+            let char = char.unwrap();
+            if let ';' = char {
+                break;
+            }
+
+            end += 1;
+        }
+
+        // boundary starts with '--'
+        Some(format!("--{}", &content_type[start..end]))
+    } else {
+        None
+    }
+}
+
+fn write_bytes(file: &mut File, bytes: Bytes) -> Result<(), std::io::Error> {
+    file.write_all(bytes.to_vec().as_slice())
+}
+
 async fn handle_post(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     let (parts, data) = req.into_parts();
-    let host = parts.headers.get("host");
-    let len = parts.headers.get("content-length");
-    let rest = parts.headers.get("content-type");
-    println!("{rest:?}");
-    todo!()
+    let _host = parts.headers.get("host").unwrap();
+
+
+    // we only want to accept forms
+    // * https://www.iana.org/assignments/media-types/media-types.xhtml
+    let content_type = parts.headers.get("content-type").unwrap();
+    let content_type_str = content_type.to_str().unwrap();
+    if let Some(i) = content_type_str.find(";"){
+        assert_eq!("multipart/form-data", &content_type_str[..i]);
+    } else {
+        panic!("how are we here");
+    }
+
+    let boundary = boundary_from_content_type(&content_type);
+    if let None = boundary {
+        panic!("no boundary field");
+    }
+
+    let boundary = boundary.unwrap();
+    let boundary_len = boundary.len();
+
+    // TODO db: hash files => filename, (!!! KEEP EXTENSION !!!)
+    let mut file = File::create("./arbitrary/what.txt").unwrap();
+
+    // TODO: proper size checking, this is currently just an estimated upper bound
+    let size_hint = data.size_hint();
+    match size_hint.upper() {
+        Some(size) => {
+            if size > FILESIZE_MAX {
+                todo!("dropped packet, size hint upper bound too big: `{size}`");
+            }
+        },
+        None => {
+            todo!("dropped packet, size hint upper bound non-existent");
+        },
+    }
+
+    // read entire body of the request into memory
+    let body = data.collect().await.unwrap().to_bytes();
+
+    // verify boundary matches what was in header
+    assert_eq!(body.slice(0..boundary_len), boundary);
+
+    // skip header included in body by finding first instance of '\r\n\r\n'
+    // NOTE: what if there is '\r\n\r\n' sent in the headers?
+
+    // (progress : expect) = (0 | 2 : '\r') || (1 : '\n')
+    let mut progress = 0;
+    let mut expect = b'\r';
+    let mut header_end: Option<usize> = None;
+    for (i, byte) in body.slice(boundary_len..).iter().enumerate() {
+        if *byte == expect {
+            progress += 1;
+            expect = if progress == 2 {
+                b'\r'
+            } else {
+                b'\n'
+            }
+        } else {
+            progress = 0;
+            expect = b'\r';
+        }
+
+        if progress == 3 {
+            header_end = Some(i);
+            break;
+        }
+    }
+
+    if let None = header_end {
+        panic!("no header end found");
+    }
+
+    let header_end = header_end.unwrap() + boundary_len + 2;
+
+    // TODO db: db entry, probably just hash files and store on fs with the
+    // hashes as an entry. binary blobs wouldnt be that bad if it stays small
+    // but if there was ever real amounts of traffic this would be an issue
+    // * https://dba.stackexchange.com/questions/2445/should-binary-files-be-stored-in-the-database
+
+    // write to file
+    // 6 is from format of '\r\n<boundary>--\r\n'
+    let rest_body = body.slice(header_end..(body.len() - boundary_len - 6));
+    // println!("{:?}", infer::get(&rest_body));
+    write_bytes(&mut file, rest_body).unwrap();
+
+    Ok(Response::new(Full::new(template_content!(404))))
 }
 
 async fn handle_request(req: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    print_packet(&req);
     let method = req.method();
     match *method {
         Method::GET => {
@@ -102,7 +225,7 @@ async fn handle_request(req: Request<hyper::body::Incoming>) -> Result<Response<
             handle_post(req).await
         },
         _ => {
-            panic!("unsupported method: {method:?}")
+            panic!("unsupported method: {method:?}");
         }
     }
 }
@@ -115,8 +238,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     loop {
         let (stream, _) = listener.accept().await?;
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
+        // Use an adapter to access something implementing `tokio::io` traits
+        // as if they implement `hyper::rt` IO traits.
         // NOTE: ?????????
         let io = TokioIo::new(stream);
 
